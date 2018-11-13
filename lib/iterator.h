c2bb295a3ce5d7e72136e6c2e9b6080c2d8217ce
Billy <billy.bouchard03@gmail.com>
2018-11-12 21:56:26 -0500

    Fix implementation of ++ and --

--- lib/iterator.h
+++ lib/iterator.h
@@ -12,15 +12,15 @@ public:
     inline void operator=(const Iterator<T> & it) {_val = it._val; _next = it._next; _last = it._last;}
     inline ~Iterator() {};
 
-    inline Iterator & operator++() {this = _next;}
-    Iterator & operator--() {this = _last;}
+    inline Iterator & operator++();
+    inline Iterator & operator--();
+
     T & operator*() {return _val;}
 
     Iterator<T> & getNext() const {return *_next;}
     Iterator<T> & getLast() const {return *_last;}
-    Iterator<T> & setNext(Iterator<T> * next) {_next = next; next->_last = this; return *this;}
-    Iterator<T> & setLast(Iterator<T> * last) {_last = last; last->_next = this; return *this;}
-    
+    Iterator<T> & setNext(Iterator<T> * next) {_next = next; next->_last = this; return *_next;}
+    Iterator<T> & setLast(Iterator<T> * last) {_last = last; last->_next = this; return *_last;}
     
     void operate(void (* fptr)(T val));
     Iterator<T> * unlink() {_next->_last = _last; _last->_next = _next; return _next;}
@@ -30,4 +30,21 @@ private:
     Iterator<T> * _next;
     Iterator<T> * _last;
 };
+
+template<class T>
+inline Iterator<T> & Iterator<T>::operator--(){
+    Iterator * n = new Iterator(*this); 
+    *this = *_last;
+    _next = n; 
+    return *this;
+}
+
+template<class T>
+inline Iterator<T> & Iterator<T>::operator++() {
+    Iterator * n = new Iterator(*this); 
+    *this = *_next;
+    _last = n; 
+    return *this;
+}
+
 #endif // !ITERATOR_H
